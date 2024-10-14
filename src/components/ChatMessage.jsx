e803e747fe0a4aa66d8c1496ea6900cb2a49f97a
Zidane Putra Andriant <zidaneputraandriant77@gmail.com>
2024-10-14 15:38:15 +0700

    fix

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,31 +1,60 @@
 import { Box, Card, CardContent, Typography } from "@mui/material";
+import { useMemo } from "react";
 
-export default function ChatMessage({ username, message, isOwnMessage, timestamp }) {
-    const formattedTime = new Date(timestamp).toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    });
+const stringToColor = (string) => {
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).slice(-2);
+  }
+  return color;
+};
 
-    return (
-        <Box display="flex" justifyContent={isOwnMessage ? "flex-end" : "flex-start"} m={2}>
-            <Card sx={{ backgroundColor: isOwnMessage ? 'blue' : 'black' }}>
-                <CardContent>
-                    {!isOwnMessage && (
-                        <Typography variant="body2" color="primary">
-                            {username}
-                        </Typography>
-                    )}
-                    <Box display="flex" justifyContent="space-between" alignItems="flex-end">
-                        <Typography variant="body1" color="white">
-                            {message}
-                        </Typography>
-                        <Typography variant="caption" color="gray" sx={{ paddingLeft: 2 }}>
-                            {formattedTime}
-                        </Typography>
-                    </Box>
-                </CardContent>
-            </Card>
-        </Box>
-    );
+export default function ChatMessage({
+  username,
+  message,
+  isOwnMessage,
+  timestamp,
+}) {
+  const formattedTime = new Date(timestamp).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  const usernameColor = useMemo(() => stringToColor(username), [username]);
+
+  return (
+    <Box
+      display="flex"
+      justifyContent={isOwnMessage ? "flex-end" : "flex-start"}
+      m={2}
+    >
+      <Card sx={{ backgroundColor: isOwnMessage ? "blue" : "black" }}>
+        <CardContent>
+          {!isOwnMessage && (
+            <Typography variant="body2" sx={{ color: usernameColor }}>
+              {username}
+            </Typography>
+          )}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="flex-end"
+          >
+            <Typography variant="body1" color="white">
+              {message}
+            </Typography>
+            <Typography variant="caption" color="gray" sx={{ paddingLeft: 2 }}>
+              {formattedTime}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,140 @@
 import ChatMessage from "@/components/ChatMessage";
 import {
-    Avatar,
-    Box,
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    Divider,
-    TextField,
-    Typography
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Typography,
 } from "@mui/material";
-import { useEffect, useState } from 'react';
-import useSocket from '../hooks/useSocket';
+import { useEffect, useState } from "react";
+import useSocket from "../hooks/useSocket";
 
 export default function Home() {
-    const [username, setUsername] = useState('');
-    const [isUsernameSet, setIsUsernameSet] = useState(false);
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const socket = useSocket();
+  const [username, setUsername] = useState("");
+  const [isUsernameSet, setIsUsernameSet] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const socket = useSocket();
 
-    useEffect(() => {
-        if (socket) {
-            socket.on('message', (msg) => {
-                console.log('Received message:', msg);
-                setMessages((prevMessages) => [...prevMessages, msg]);
-            });
-        }
-    }, [socket]);
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (msg) => {
+        console.log("Received message:", msg);
+        setMessages((prevMessages) => [...prevMessages, msg]);
+      });
+    }
+  }, [socket]);
 
-    const handleSetUsername = () => {
-        if (username.trim()) {
-            setIsUsernameSet(true);
-        }
-    };
+  const handleSetUsername = () => {
+    if (username.trim()) {
+      setIsUsernameSet(true);
+    }
+  };
 
-    const sendMessage = () => {
-        if (message && socket) {
-            console.log('Sending message:', { username, message });
-            const timestamp = new Date().toISOString();
-            socket.emit('message', { username, message, timestamp });
-            setMessage('');
-        }
-    };
+  const sendMessage = () => {
+    if (message && socket) {
+      console.log("Sending message:", { username, message });
+      const timestamp = new Date().toISOString();
+      socket.emit("message", { username, message, timestamp });
+      setMessage("");
+    }
+  };
 
-    return (
-        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={4} height="100vh">
-            {!isUsernameSet ? (
-                <Card sx={{ minWidth: 300 }}>
-                    <CardHeader title={<Typography variant="h6" fontWeight="bold">Enter Your Username</Typography>} />
-                    <Divider />
-                    <CardContent>
-                        <TextField
-                            type="text"
-                            fullWidth
-                            label="Username"
-                            variant="outlined"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </CardContent>
-                    <Box p={2}>
-                        <Button fullWidth variant="contained" color="primary" onClick={handleSetUsername}>
-                            <Typography fontWeight="bold">Join Chat</Typography>
-                        </Button>
-                    </Box>
-                </Card>
-            ) : (
-                <Card sx={{ width: '100%', height: '100%' }}>
-                    <CardHeader
-                        avatar={<Avatar />}
-                        title={<Typography variant="h6" fontWeight="bold">Group Chat</Typography>}
-                    />
-                    <Divider />
-                    <CardContent sx={{ flex: 1 }}>
-                        {messages.map((msg, index) => (
-                            <ChatMessage
-                                key={index}
-                                username={msg.username}
-                                message={msg.message}
-                                timestamp={msg.timestamp}
-                                isOwnMessage={msg.username === username}
-                            />
-                        ))}
-                    </CardContent>
-                    <Box display="flex" p={2}>
-                        <TextField
-                            type="text"
-                            fullWidth
-                            label="Message"
-                            variant="outlined"
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                        />
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={sendMessage}
-                            sx={{ marginLeft: 2, borderRadius: '50%' }}
-                        >
-                            Send
-                        </Button>
-                    </Box>
-                </Card>
-            )}
-        </Box>
-    );
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      p={4}
+      height="100vh"
+    >
+      {!isUsernameSet ? (
+        <Card sx={{ minWidth: 300 }}>
+          <CardHeader
+            title={
+              <Typography variant="h6" fontWeight="bold">
+                Enter Your Username
+              </Typography>
+            }
+          />
+          <Divider />
+          <CardContent>
+            <TextField
+              type="text"
+              fullWidth
+              label="Username"
+              variant="outlined"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </CardContent>
+          <Box p={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleSetUsername}
+            >
+              <Typography fontWeight="bold">Join Chat</Typography>
+            </Button>
+          </Box>
+        </Card>
+      ) : (
+        <Card
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <CardHeader
+            avatar={<Avatar />}
+            title={
+              <Typography variant="h6" fontWeight="bold">
+                Group Chat
+              </Typography>
+            }
+          />
+          <Divider />
+          {/* Bagian pesan dibuat scrollable */}
+          <CardContent sx={{ flex: 1, overflowY: "auto" }}>
+            {messages.map((msg, index) => (
+              <ChatMessage
+                key={index}
+                username={msg.username}
+                message={msg.message}
+                timestamp={msg.timestamp}
+                isOwnMessage={msg.username === username}
+              />
+            ))}
+          </CardContent>
+          {/* Bagian input dan tombol tetap di bawah */}
+          <Box display="flex" p={2} sx={{ borderTop: "1px solid #e0e0e0" }}>
+            <TextField
+              type="text"
+              fullWidth
+              label="Message"
+              variant="outlined"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={sendMessage}
+              sx={{ marginLeft: 2, borderRadius: "50%" }}
+            >
+              Send
+            </Button>
+          </Box>
+        </Card>
+      )}
+    </Box>
+  );
 }
